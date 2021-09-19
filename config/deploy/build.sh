abf554c1bc01384affdf807d71808cf891102af6
deepak <dk5f95@gmail.com>
2021-09-20 02:23:05 +0530

    sleep

--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -4,8 +4,7 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=docker-registry.cityapl.com/cityapl_product_service
 DOCKER_CRED=cityapl@123
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
-echo "Docker Stop start "
 sudo docker ps -q --filter ancestor=${DOCKER_IMAGE} | xargs -r sudo docker stop
-echo "Docker Stop start "
+do sleep 5; done
 sudo docker pull ${DOCKER_IMAGE}
 sudo docker run -d -p 5000:5000 ${DOCKER_IMAGE}
