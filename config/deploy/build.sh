f205d979ce8c5c3bc48667d227f7f07250f487a9
deepak <dk5f95@gmail.com>
2021-09-20 01:54:20 +0530

    changed in build file

--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -4,6 +4,6 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=docker-registry.cityapl.com/cityapl_product_service
 DOCKER_CRED=cityapl@123
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
-sudo docker pull $DOCKER_IMAGE
 sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
+sudo docker pull $DOCKER_IMAGE
 sudo docker run -d -p 5000:5000 $DOCKER_IMAGE
