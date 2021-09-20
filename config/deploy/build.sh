e6aeea2cc5a8eccadb1aa5c7a7eca1ce06269a9c
deepak <dk5f95@gmail.com>
2021-09-20 23:04:29 +0530

    .

--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -4,7 +4,7 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=docker-registry.cityapl.com/cityapl_product_service
 DOCKER_CRED=cityapl@123
 
-sudo docker stop $(sudo docker ps -q --filter ancestor={DOCKER_IMAGE} )
+# sudo docker stop $(sudo docker ps -q --filter ancestor={DOCKER_IMAGE} )
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
 sudo docker pull ${DOCKER_IMAGE}
 # sudo docker run -d -p 5000:5000 ${DOCKER_IMAGE}
