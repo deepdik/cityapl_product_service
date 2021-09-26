87c61a2a9ff8f06a75ca859663075a17a843cacd
deepak <dk5f95@gmail.com>
2021-09-26 16:00:01 +0530

    .

--- config/deploy/before_build.sh
+++ config/deploy/before_build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# if any of the commands in your code fails for any reason, the entire script fails
+set -o errexit
+# exits if any of your variables is not set
+set -o nounset
+
 echo "Starting before build.."
 
 
--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# if any of the commands in your code fails for any reason, the entire script fails
+set -o errexit
+# exits if any of your variables is not set
+set -o nounset
+
 
 echo "Starting to deploy docker image.."
-SERVICE_NAME = cityapl_product_service
-DOCKER_IMAGE_WEB=docker-registry.cityapl.com/${SERVICE_NAME}
 
+SERVICE_NAME=cityapl_product_service
 DOCKER_CRED=cityapl@123
-GIT_CRED = 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
+GIT_CRED=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
 
-rm -rf ${SERVICE_NAME}
+sudo rm -rf ${SERVICE_NAME}
 git clone https://deepdik:${GIT_CRED}@github.com/deepdik/${SERVICE_NAME}.git
 
 cd ${SERVICE_NAME}
 
-sudo docker-compose down --remove-orphans --rmi 
+sudo docker-compose down
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
 sudo docker-compose pull
 sudo docker-compose up -d
+
+exit
 
 ###########################################################
