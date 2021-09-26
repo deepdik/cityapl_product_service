aebc259bbfd0a4a53aca855c35dbee6e6e179412
deepak <dk5f95@gmail.com>
2021-09-26 12:43:51 +0530

    .

--- config/deploy/before_build.sh
+++ config/deploy/before_build.sh
@@ -5,5 +5,10 @@ echo "Starting before build.."
 
 # install docker, docker-compose and other dependency
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
 sudo groupadd docker
 sudo usermod -aG docker $USER
