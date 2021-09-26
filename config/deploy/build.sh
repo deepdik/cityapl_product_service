98cef9d30978a8b86eb88c305036df080eb382a8
deepak <dk5f95@gmail.com>
2021-09-26 13:00:53 +0530

    .

--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -6,7 +6,9 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=docker-registry.cityapl.com/cityapl_product_service
 DOCKER_CRED=cityapl@123
 
-sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r sudo docker stop
+# sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r sudo docker stop
+sudo docker-compose stop
+sudo docker-compose rm -f
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
 sudo docker pull ${DOCKER_IMAGE}
 
@@ -14,10 +16,9 @@ sudo docker pull ${DOCKER_IMAGE}
 
 ############### config for docker-compose ##################
 
+sudo docker-compose pull 
 sudo docker run --rm --entrypoint cat ${DOCKER_IMAGE} docker-compose.yml > docker-compose.yml
 # sudo docker-compose stop
-docker-compose stop
-docker-compose rm -f
-docker-compose pull   
-docker-compose up -d
+  
+sudo docker-compose up -d
 
