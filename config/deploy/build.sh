55a1c27a849ebac8d40d5d7e80eb040f5286a31f
deepak <dk5f95@gmail.com>
2021-09-26 15:30:57 +0530

    .

--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -12,6 +12,8 @@ DOCKER_CRED=cityapl@123
 git clone https://deepdik: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@github.com/deepdik/cityapl_product_service.git
 
 cd cityapl_product_service
+
+sudo docker-compose down --remove-orphans --rmi 
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
 sudo docker-compose pull
 sudo docker-compose up
