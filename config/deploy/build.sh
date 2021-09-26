7cbfc7c93d94b51aa9556c8d3aeea81b97845f52
deepak <dk5f95@gmail.com>
2021-09-26 13:05:01 +0530

    .

--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -10,7 +10,7 @@ DOCKER_CRED=cityapl@123
 sudo docker-compose stop
 sudo docker-compose rm -f
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
-sudo docker pull ${DOCKER_IMAGE}
+# sudo docker pull ${DOCKER_IMAGE}
 
 ###########################################################
 
@@ -20,5 +20,5 @@ sudo docker-compose pull
 sudo docker run --rm --entrypoint cat ${DOCKER_IMAGE} docker-compose.yml > docker-compose.yml
 # sudo docker-compose stop
   
-sudo docker-compose up -d
+sudo docker-compose up
 
