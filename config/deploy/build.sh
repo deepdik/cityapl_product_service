6c6533c7aab616ab46de623b47ecdaec5eb952c8
deepak <dk5f95@gmail.com>
2021-09-26 15:47:25 +0530

    .

--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
 
-########### config when only one docker image #############
 echo "Starting to deploy docker image.."
-DOCKER_IMAGE_WEB=docker-registry.cityapl.com/cityapl_product_service
+SERVICE_NAME = cityapl_product_service
+DOCKER_IMAGE_WEB=docker-registry.cityapl.com/${SERVICE_NAME}
 
 DOCKER_CRED=cityapl@123
+GIT_CRED = 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
 
-# sudo docker ps -q --filter ancestor=$DOCKER_IMAGE_WEB | xargs -r sudo docker rm -f
+rm -rf ${SERVICE_NAME}
+git clone https://deepdik:${GIT_CRED}@github.com/deepdik/${SERVICE_NAME}.git
 
-git clone https://deepdik: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@github.com/deepdik/cityapl_product_service.git
-
-cd cityapl_product_service
+cd ${SERVICE_NAME}
 
 sudo docker-compose down --remove-orphans --rmi 
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
 sudo docker-compose pull
-sudo docker-compose up
+sudo docker-compose up -d
 
 ###########################################################
-
-# sudo docker run --rm --entrypoint cat ${DOCKER_IMAGE_WEB} docker-compose.yml > docker-compose.yml
-# sudo docker-compose pull 
-# # sudo docker-compose stop
-  
-# sudo docker-compose up redis
-# sudo docker run  -p 5000:5000 ${DOCKER_IMAGE_WEB}
-# sudo docker run  ${DOCKER_IMAGE_CELERY}
-# sudo docker run  ${DOCKER_IMAGE_BEAT}
-# sudo docker run  ${DOCKER_IMAGE_FLOWER}
