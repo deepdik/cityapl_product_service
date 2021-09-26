27776e50e1f765487acc6727b94f728a7c1f2038
deepak <dk5f95@gmail.com>
2021-09-26 13:10:05 +0530

    .

--- config/deploy/build.sh
+++ config/deploy/build.sh
@@ -7,7 +7,7 @@ DOCKER_IMAGE=docker-registry.cityapl.com/cityapl_product_service
 DOCKER_CRED=cityapl@123
 
 # sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r sudo docker stop
-sudo docker-compose stop
+sudo docker compose down
 sudo docker-compose rm -f
 sudo docker login https://docker-registry.cityapl.com -u cityapl_docker_registry -p ${DOCKER_CRED}
 # sudo docker pull ${DOCKER_IMAGE}
